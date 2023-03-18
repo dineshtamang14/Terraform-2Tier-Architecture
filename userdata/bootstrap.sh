73e621008480b002b5fac3d6c64294fe81540a37
Dinesh Tamang <dineshtamang7263@gmail.com>
2023-03-18 15:01:55 +0530

    updated

--- userdata/bootstrap.sh
+++ userdata/bootstrap.sh
@@ -9,44 +9,76 @@ read -s -p "Enter RDS password: " db_password
 # Install and configure WordPress
 sudo su
 apt update && apt upgrade -y
-apt install nginx php-fpm php-mysql php-curl php-dom php-mbstring php-imagick php-zip php-gd wget -y
+apt install nginx php-fpm php-mysql php-curl php-dom php-mbstring php-imagick php-zip php-gd wget unzip -y
+apt install php-imagick php7.4-fpm php7.4-mbstring php7.4-bcmath php7.4-xml php7.4-mysql php7.4-common php7.4-gd php7.4-json php7.4-cli php7.4-curl php7.4-zip -y
+mkdir -p /usr/share/nginx
 
-cat > /etc/nginx/sites-available/wordpress.conf <<EOF
+cat > /etc/nginx/conf.d/wordpress.conf <<EOF
 server {
-	listen 80 default_server;
-	listen [::]:80 default_server;
+  listen 80;
+  listen [::]:80;
+  server_name _;
+  root /usr/share/nginx/wordpress;
+  index index.php index.html index.htm index.nginx-debian.html;
 
-	root /var/www/wordpress;
+  location / {
+    try_files $uri $uri/ /index.php;
+  }
 
-	# Add index.php to the list if you are using PHP
-	index index.php;
+   location ~ ^/wp-json/ {
+     rewrite ^/wp-json/(.*?)$ /?rest_route=/$1 last;
+   }
 
-	server_name _;
+  location ~* /wp-sitemap.*\.xml {
+    try_files $uri $uri/ /index.php$is_args$args;
+  }
+  client_max_body_size 20M;
 
-	location / {
-		try_files $uri $uri/ =404;
-	}
+  location ~ \.php$ {
+    fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+    fastcgi_param SCRIPT_FILENAME $document_root$fastcgi_script_name;
+    include fastcgi_params;
+    include snippets/fastcgi-php.conf;
+    fastcgi_buffers 1024 4k;
+    fastcgi_buffer_size 128k;
 
-	location ~ \.php$ {
-		include snippets/fastcgi-php.conf;
-	
-		# With php-fpm (or other unix sockets):
-		fastcgi_pass unix:/var/run/php/php7.4-fpm.sock;
-		# With php-cgi (or other tcp sockets):
-		# fastcgi_pass 127.0.0.1:9000;
-	}
+    # Add headers to serve security related headers
+    add_header X-Content-Type-Options nosniff;
+    add_header X-XSS-Protection "1; mode=block";
+    add_header X-Permitted-Cross-Domain-Policies none;
+    add_header X-Frame-Options "SAMEORIGIN";
+  }
+
+  #enable gzip compression
+  gzip on;
+  gzip_vary on;
+  gzip_min_length 1000;
+  gzip_comp_level 5;
+  gzip_types application/json text/css application/x-javascript application/javascript image/svg+xml;
+  gzip_proxied any;
+
+  # A long browser cache lifetime can speed up repeat visits to your page
+  location ~* \.(jpg|jpeg|gif|png|webp|svg|woff|woff2|ttf|css|js|ico|xml)$ {
+       access_log        off;
+       log_not_found     off;
+       expires           360d;
+  }
+
+  # disable access to hidden files
+  location ~ /\.ht {
+      access_log off;
+      log_not_found off;
+      deny all;
+  }
+
 }
 EOF
 
-ln -s /etc/nginx/sites-available/wordpress.conf /etc/nginx/sites-enabled/
-rm /etc/nginx/sites-available/default
-rm /etc/nginx/sites-enabled/default
-cd /var/www
-sudo wget https://wordpress.org/latest.tar.gz
-sudo tar -xzvf latest.tar.gz
-sudo rm latest.tar.gz
+wget https://wordpress.org/latest.zip
+unzip latest.zip -d /usr/share/nginx/
+sudo rm latest.zip
 
-cat > /var/www/wordpress/wp-config.php <<EOF
+cat > /usr/share/nginx/wordpress/wp-config.php <<EOF
 <?php
 define( 'DB_NAME', '${db_name}' );
 define( 'DB_USER', '${db_username}' );
@@ -56,8 +88,8 @@ define( 'DB_CHARSET', 'utf8' );
 define( 'DB_COLLATE', 'utf8_unicode_ci );
 EOF
 
-rm wordpress/wp-config-sample.php
-chown -R www-data:www-data wordpress
-find wordpress/ -type d -exec chmod 755 {} \;
-find wordpress/ -type f -exec chmod 644 {} \;
-systemctl restart nginx
+rm /usr/share/nginx/wordpress/wp-config-sample.php
+chown www-data:www-data /usr/share/nginx/wordpress/ -R
+nginx -t
+systemctl reload nginx
+systemctl reload php7.4-fpm
